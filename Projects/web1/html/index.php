e9bf7c25f6f8fdb85a5ab1e9bab29a20275d493c
OfilioHA <herdocia20@gmail.com>
2021-11-26 14:29:14 -0600

    Pagina de PHPSlim sin funcionar.

---
+++ Projects/web1/html/index.php
@@ -0,0 +1,25 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Exception\NotFoundException;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+$app->run();
+
+
+?>
